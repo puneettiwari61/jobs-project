7fdde2709ca964cd4b8450a46e3df15abb7b97be
Puneet <puneettiwari61@gmail.com>
2020-04-23 02:29:43 +0530

    candiate signup and login route

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,10 +1,30 @@
+var User = require("../models/users");
+var auth = require("../routes/auth");
 module.exports = {
   signUp: async (req, res) => {
     try {
       console.log(req.body, "coming from users signup controller");
-      res.send("employers signup api");
+      var user = await User.create(req.body);
+      res.json({ success: true, user });
     } catch (err) {
       console.log(err);
+      res.json({ success: false, err });
+    }
+  },
+  login: async (req, res) => {
+    try {
+      console.log(req.body, "coming from users signup controller");
+      var user = await User.findOne({ email: req.body.email });
+      if (!user)
+        return res.json({ success: false, msg: "incorrect credentials" });
+      if (!user.verifyPassword(req.body.password)) {
+        return res.json({ success: false, msg: "incorrect password" });
+      }
+      var token = await auth.generateJWT(user);
+      res.json({ success: true, user, token });
+    } catch (err) {
+      console.log(err);
+      res.json({ success: false, err });
     }
   }
 };
--- routes/users/index.js
+++ routes/users/index.js
@@ -2,7 +2,10 @@ var express = require("express");
 var router = express.Router();
 var users = require("../../controllers/usersController");
 
-/* GET users listing. */
-router.get("/signup", users.signUp);
+/* Signup User. */
+router.post("/signup", users.signUp);
+
+/* login User. */
+router.post("/login", users.login);
 
 module.exports = router;
