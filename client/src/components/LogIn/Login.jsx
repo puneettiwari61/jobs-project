2473695aedb6df69984e28d5612c7483ea789471
piyushsi <piyushsinha2309@gmail.com>
2020-04-23 15:39:05 +0530

    LoadingPage and LoginPage

--- client/src/components/LogIn/Login.jsx
+++ client/src/components/LogIn/Login.jsx
@@ -3,7 +3,28 @@ import "./login.scss";
 
 class Login extends Component {
   render() {
-    return <></>;
+    return <>
+    <div class="login-box">
+	<h2>Login</h2>
+	<form action="">
+		<div class="user-box">
+			<input type="text" name="" required=""/>
+			<label>Username</label>
+		</div>
+		<div class="user-box">
+			<input type="password"/>
+			<label for="">Password</label>
+		</div>
+		<a href="">
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			Login
+		</a>
+	</form>
+</div>
+</>;
   }
 }
 
