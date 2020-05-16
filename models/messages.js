c18ff408e08305f9ea0989b428f4f7c6090cc9db
koushikshetty94 <koushikshetty94@gmail.com>
2020-05-16 17:56:16 +0530

    conversation and message schema

---
+++ models/messages.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var messageSchema = new Schema({
+    employerId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Employer'
+    },
+    candidateId: {
+        type: Schema.Types.ObjectId;
+        ref: 'Candidate'
+    },
+    senderType:{
+        type : String
+    },
+    senderId: {
+        type: String
+    },
+    receiverId: {
+        type: String
+    },
+    message: {
+        type: String,
+        required : true
+    },
+    isSeen:{
+        type: Boolean,
+        default: false
+    }
+})
+
+module.exports = mongoose.model("Message",messageSchema);
