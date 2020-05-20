2c82ae7497670bd45c6d444a96619dd42f560da5
piyushsi <piyushsinha2309@gmail.com>
2020-05-21 04:11:17 +0530

    Landing page complete-learn more required

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -7,12 +7,24 @@ import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import WorkOutlineIcon from '@material-ui/icons/WorkOutline';
 import SchoolIcon from '@material-ui/icons/School';
+import PermContactCalendarIcon from '@material-ui/icons/PermContactCalendar';
+import { geolocated } from 'react-geolocated';
+import Axios from 'axios';
 
 class LandingPage extends Component {
 	constructor() {
 		super();
-		this.state = { active: true };
+		this.state = { active: true, address: null };
 	}
+	displayLocation = (latitude, longitude) => {
+		Axios.get(
+			`https://us1.locationiq.com/v1/reverse.php?key=8933ef90f0be62&lat=${latitude}&lon=${longitude}&tag=countries&radius=400000&format=json`
+		)
+			.then((res) => {
+				this.setState({ address: res });
+			})
+			.catch((err) => console.log(err));
+	};
 	render() {
 		let employer = this.props.employer;
 		let candidate = this.props.candidate;
@@ -29,97 +41,166 @@ class LandingPage extends Component {
 				<div class="page">
 					<header>
 						<main className="header">
-							{/* <div class="logo">
-								<img src="https://i.ibb.co/8Kc0TQC/rec.png" alt="logo" />
-								Remote
-							</div> */}
+							<div class="logo">
+								<img
+									src="https://i.ibb.co/8Kc0TQC/rec.png"
+									alt="logo"
+									onClick={() =>
+										this.displayLocation(this.props.coords.latitude, this.props.coords.longitude)
+									}
+								/>
+								{!this.props.isGeolocationAvailable
+									? 'Your browser does not support Geolocation'
+									: !this.props.isGeolocationEnabled
+									? 'Geolocation is not enabled'
+									: this.props.coords && this.state.address
+									? `${this.state.address.request.response
+											.split('county')[1]
+											.split(',')[0]
+											.substr(
+												3,
+												this.state.address.request.response.split('county')[1].split(',')[0]
+													.length - 4
+											)}`
+									: 'Location'}
+							</div>
 							<ul>
-								<li>Jobs</li>
-								<li>Services</li>
-								<li>Contact</li>
+								<a href="#">
+									<li>Home</li>
+								</a>
+								<a href="#services">
+									<li>Services</li>
+								</a>
+								<a href="#contact">
+									<li>Contact</li>
+								</a>
 							</ul>
 						</main>
 
 						<main className="main">
 							<div class="left_side">
-								<h1>Are you a skilled individual looking to kick start your career?<br/>OR<br/>Are you an employer looking to hire the best?</h1>
+								<h1>The best solution to find Jobs</h1>
 								<h4>
-									Do you need the coolest and the most awesome platform to manage your project?! You
-									have walked to the right place!
+									Are you a skilled individual looking to kick start your career?
+									<br />
+									OR
+									<br />
+									Are you an employer looking to hire the best?
 								</h4>
-								{/* <button class="main_btn">Get Started</button>
+								<a href="#services">
+									<button class="main_btn">Get Started</button>
+								</a>
 								<button class="outline_btn">Learn More</button>
 								<div class="social">
-									<button class="outline_btn">
-										<TwitterIcon />
-									</button>
-									<button class="outline_btn">
-										<InstagramIcon />
-									</button>
-									<button class="main_btn">
-										<LinkedInIcon />
-									</button>
-								</div> */}
+									<a href="https://twitter.com/AltCampus" target="_blank">
+										<button class="outline_btn">
+											<TwitterIcon />
+										</button>
+									</a>
+									<a href="https://www.instagram.com/altcampus/" target="_blank">
+										<button class="outline_btn">
+											<InstagramIcon />
+										</button>
+									</a>
+									<a href="https://www.linkedin.com/company/altcampus/" target="_blank">
+										<button class="main_btn">
+											<LinkedInIcon />
+										</button>
+									</a>
+								</div>
 							</div>
 							<div class="right_side">
 								<div></div>
 							</div>
 						</main>
-						{/* <p class="show_more">
-							See how we help great companies with their works <i class="fa fa-play-circle-o"></i>
-						</p> */}
+						<div id="services">
+							<p class="show_more">
+								Anyone can:
+								<br />
+								-Gain or improve skills with practice or short-term training.
+								<br />
+								-Look into state-approved apprenticeship programs to gain skills and earn a salary.
+								<br />
+								-Contact a local public library about free classes. Many offer basic computer training
+								and job skill classes.
+								<br />
+								-Review Occupational Skills and Employability Skills for more ways to expand your
+								skills.
+								<br />
+							</p>
 
-						<section className="section">
-							<div class="item">
-								<button class="main_btn"
-								onClick={()=>this.props.history.push("/employers/login")}>
-									<WorkOutlineIcon />
-								</button>
-								<h5>Employer Login</h5>
-							
-							</div>
-							<div class="item">
-								<button class="main_btn second"
-								onClick={()=>this.props.history.push("/candidates/login")}>
-									<SchoolIcon />
-								</button>
-								<h5>Candidate Login</h5>
-							</div>
-							<div class="text_content">
-								<h2>About us!</h2>
-								<p>
-									Steves Jobs is a great bridge that helps in connecting highly skilled candidates and great employers.
-								</p>
-								<button class="outline_btn">Learn More</button>
-								<button class="outline_btn">
-									<i class="fa fa-play"></i>
-								</button>
-							</div>
-						</section>
+							<section className="section">
+								<div class="item">
+									<button class="main_btn">
+										<WorkOutlineIcon />
+									</button>
+									<h5>Employer</h5>
+									<a href="/employers/signup">Register</a>
+									<p>Employers can get best Candidates as per Skills Rating.</p>
+								</div>
+								<div class="item">
+									<button class="main_btn second">
+										<SchoolIcon />
+									</button>
+									<h5>Candidate</h5>
+									<a href="/candidates/signup">Register</a>
+									<p>
+										Candidates can contact with Employer as per Skills Rating.
+										<br />
+									</p>
+								</div>
+								<div class="text_content">
+									<h2>Why choose us!</h2>
+									<p>
+										We connect you directly with Employer if your Skills Rating is best among your
+										Competitors. This is a Job Portal site or app that enables you to find Jobs as
+										per your Skills.
+									</p>
+									<button class="outline_btn">Learn More</button>
+									<br />
+									<a href="#contact">
+										<button class="outline_btn">
+											<PermContactCalendarIcon />
+										</button>
+									</a>
+								</div>
+							</section>
+						</div>
 					</header>
-					<div class="hero-box-container">
+					<div id="contact" class="hero-box-container">
 						<a href="" class="hero-box">
 							<span class="hero-box__circle hero-box__circle--blue"></span>
-							<h2 class="hero-box__title">Sell your device</h2>
+							<h2 class="hero-box__title">About Us</h2>
 							<p class="hero-box__body">
-								Get instant quote
-								<br /> and ship device for free
+								AltCampus students.
+								<br /> Location: Dharamshala, HP, India
+								<br />
+								AltCampus, The Alternative to College That You Wish Existed.
 							</p>
 						</a>
 						<a href="" class="hero-box">
 							<span class="hero-box__circle hero-box__circle--green"></span>
-							<h2 class="hero-box__title">Repair</h2>
+							<h2 class="hero-box__title">
+								<img
+									className="logo_job"
+									src="https://lh3.googleusercontent.com/pw/ACtC-3ewkA_FaRzGusIEIH6ra-9KVBIjNOvaH_qVaxcOAuppSEWKvhw6WwOQXii595yi06hnBrCN6fbzKHUox-j5AgvaH9M577VK5PggX0C8h-zT6iaeq7bdt1S7Vq2jMjniaH8GgUhGrtMBVuMN0SFA44-b=w659-h609-no?authuser=0"
+								/>
+							</h2>
 							<p class="hero-box__body">
-								Give a second chance
-								<br /> to your device
+								Steve's Jobs
+								<br /> Find better than best
 							</p>
 						</a>
 						<a href="" class="hero-box">
 							<span class="hero-box__circle hero-box__circle--orange"></span>
-							<h2 class="hero-box__title">Exchange</h2>
+							<h2 class="hero-box__title">Stay Connected</h2>
 							<p class="hero-box__body">
-								Refurbished and lightly used
-								<br /> device marketplace
+								<TwitterIcon />
+								<InstagramIcon />
+								<LinkedInIcon />
+								<br />
+								altstevesjobs@gmail.com
+								<br /> © Steve's Jobs, 2020-present.
 							</p>
 						</a>
 					</div>
@@ -174,4 +255,10 @@ class LandingPage extends Component {
 function mapToProps({ candidate, employer }) {
 	return { candidate, employer };
 }
-export default connect(mapToProps)(withRouter(LandingPage));
+
+export default geolocated({
+	positionOptions: {
+		enableHighAccuracy: false,
+	},
+	userDecisionTimeout: 5000,
+})(connect(mapToProps)(withRouter(LandingPage)));
